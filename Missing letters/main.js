271e82d3149e635936f7a350ece3c313f4fc3eb7
Bartek <47401641+bbombelka@users.noreply.github.com>
2019-02-24 09:35:54 +0100

    Update main.js

--- Missing letters/main.js
+++ Missing letters/main.js
@@ -1,5 +1,17 @@
-function fearNotLetter(str) {
+// Intermediate Algorithm Scripting: Missing letters by freeCodeCamp
+// https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/missing-letters
 
+// Find the missing letter in the passed letter range and return it. If all letters are present in the range, return undefined.
+
+// sfearNotLetter("abce") should return "d".
+//fearNotLetter("abcdefghjklmno") should return "i".
+//fearNotLetter("stvwx") should return "u".
+//fearNotLetter("bcdf") should return "e".
+//fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined
+
+// my solution
+
+function fearNotLetter(str) {
 
     str= str.split('')
     
@@ -8,8 +20,4 @@ function fearNotLetter(str) {
         return String.fromCharCode(((str[i+1].charCodeAt() + str[i].charCodeAt())/2))
         
     }
-
-    
-
 }
-  fearNotLetter("abcde");
