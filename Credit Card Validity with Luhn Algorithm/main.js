22da6a3691917caa51feebb72fa4da642c9d7d2f
Barty-Boy <b.bombelka@gmail.com>
2020-08-19 15:20:36 +0200

    Add input text masking

--- Credit Card Validity with Luhn Algorithm/main.js
+++ Credit Card Validity with Luhn Algorithm/main.js
@@ -15,38 +15,42 @@
 
 */
 
-const checkInputContentForValidCreditCardNumber = inputText => {
-  const removeUnnecessaryChars = text => {
+const checkInputContentForValidCreditCardNumber = (inputText) => {
+  let textWithMaskedCardNumber = inputText;
+
+  const removeUnnecessaryChars = (text) => {
     return text.replace(/[^A-Za-z\d]/g, '');
   };
 
-  const findProbableCardNumbers = text => {
+  const findProbableCardNumbers = (text) => {
     const contains16DigitNumber = /\d{16}/g.test(text);
     if (contains16DigitNumber) return text.match(/\d{16}/g);
   };
 
-  const checkBinCompliancy = probableCardNumbers => {
-    return probableCardNumbers.filter(cardNumber =>
-      binEnum.some(binNumber => cardNumber.startsWith(binNumber)),
+  const checkBinCompliancy = (probableCardNumbers) => {
+    return probableCardNumbers.filter((cardNumber) =>
+      binEnum.some((binNumber) => cardNumber.startsWith(binNumber))
     );
   };
 
-  const verifyWithLuhnAlg = probableCardNumbers => {
-    const getSumOfDigits = number => {
+  const verifyWithLuhnAlg = (probableCardNumbers) => {
+    const getSumOfDigits = (number) => {
       return String(number)
         .split('')
-        .map(number => parseInt(number))
+        .map((number) => parseInt(number))
         .reduce((acc, curr) => acc + curr);
     };
 
-    const transformFactorTwoNumbers = numbers => {
-      return numbers.map(number => {
+    const transformFactorTwoNumbers = (numbers) => {
+      return numbers.map((number) => {
         const doubledNumber = number * 2;
-        return doubledNumber > 9 ? getSumOfDigits(doubledNumber) : doubledNumber;
+        return doubledNumber > 9
+          ? getSumOfDigits(doubledNumber)
+          : doubledNumber;
       });
     };
 
-    return probableCardNumbers.map(probableCardNumber => {
+    return probableCardNumbers.map((probableCardNumber) => {
       const numbersWithFactorTwo = probableCardNumber
         .split('')
         .filter((_, index) => index % 2 === 0);
@@ -55,11 +59,32 @@ const checkInputContentForValidCreditCardNumber = inputText => {
         .filter((_, index) => index % 2 === 1);
 
       const sum = getSumOfDigits(
-        [...transformFactorTwoNumbers(numbersWithFactorTwo), ...numbersWithFactorOne].join(''),
+        [
+          ...transformFactorTwoNumbers(numbersWithFactorTwo),
+          ...numbersWithFactorOne,
+        ].join('')
       );
 
       return { probableCardNumber, valid: String(sum).endsWith('0') };
     });
+  };
+
+  const maskNumber = (cardNumber) => {
+    const mask = '**** ';
+    const secondPart = cardNumber.substring(4, 8);
+    const thirdPart = cardNumber.substring(8, 12);
+    const suffix = cardNumber.substring(12, 16);
+
+    const secondPartRegex = new RegExp(
+      `${secondPart}(?=${thirdPart}|[\\s]*${thirdPart})`
+    );
+    const thirdPartRegex = new RegExp(
+      `${thirdPart}(?=${suffix}|[\\s]*${suffix})`
+    );
+
+    textWithMaskedCardNumber = textWithMaskedCardNumber
+      .replace(secondPartRegex, ' ' + mask)
+      .replace(thirdPartRegex, mask);
   };
 
   const processedText = removeUnnecessaryChars(inputText);
@@ -72,8 +97,17 @@ const checkInputContentForValidCreditCardNumber = inputText => {
   const binCompliantNumbers = checkBinCompliancy(probableCardNumbers);
 
   if (binCompliantNumbers.length) {
-    return verifyWithLuhnAlg(binCompliantNumbers);
+    const creditCards = verifyWithLuhnAlg(binCompliantNumbers);
+    if (creditCards) {
+      const validCreditCards = creditCards.filter((card) => card.valid);
+      if (validCreditCards.length) {
+        validCreditCards.forEach(({ probableCardNumber }) =>
+          maskNumber(probableCardNumber)
+        );
+      }
+      return textWithMaskedCardNumber;
+    }
   }
 };
 
-const binEnum = ['416039', '535817', '527003', '552956', '123456'];
+const binEnum = ['416039', '535817', '527003', '552956', '123456', '424671'];
