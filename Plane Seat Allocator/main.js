d01b1209156b213a03dae436cd323b3c955ce152
Barty-Boy <b.bombelka@gmail.com>
2021-02-24 20:08:45 +0100

    Add second version of plane seat alloator

--- Plane Seat Allocator/main.js
+++ Plane Seat Allocator/main.js
@@ -27,7 +27,7 @@ function planeSeatsAllocator(numberOfRows, reservedSeats = '') {
             foundSeats++;
           }
         }
-      }||
+      }
     });
 
     iterator++;
@@ -52,4 +52,49 @@ function prepareReservedSeats(reservedSeats) {
   return rowNumbersWithReservedSeats.map((rowNumber) =>
     bookedSeats.filter((seat) => new RegExp(`^${rowNumber}(?=[A-Z])`).test(seat)).map((seat) => seat[seat.length - 1])
   );
+}
+
+
+// second version
+
+function planeSeatsAllocatorVersionTwo(numberOfRows, reservedSeats = '') {
+  if (numberOfRows < 1 || numberOfRows > 50) {
+    return;
+  }
+
+  const columns = [
+    ['A', 'B', 'C'],
+    ['D', 'E', 'F', 'G'],
+    ['H', 'J', 'K'],
+  ];
+
+  const occupiedSeats = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    occupiedSeats.push(['', '', '']);
+  }
+
+  const getColumnIndex = (letter) => columns.findIndex((col) => col.includes(letter));
+
+  const transpileSeat = (seat) => {
+    const seatLetter = seat[seat.length - 1];
+    return {
+      rowIndex: parseInt(seat) - 1,
+      columnIndex: getColumnIndex(seatLetter);
+      seatLetter,
+    };
+  };
+
+  const convertToBool = (seat, index) =>
+    index !== 1 ? Boolean(seat) : seat.includes('E' || 'F') || (seat.includes('G') && seat.includes('D'));
+
+  const transpiledSeatMap = reservedSeats.split(' ').map(transpileSeat);
+
+  transpiledSeatMap.forEach(({ rowIndex, columnIndex, seatLetter }) => {
+    occupiedSeats[rowIndex][columnIndex] += seatLetter;
+  });
+
+  return occupiedSeats
+    .map((row) => row.map(convertToBool))
+    .flat()
+    .filter((bool) => !bool).length;
 }
